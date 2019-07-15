0f88033da8355923a27c010934e9d242d789ab10
Rimil Dey <rimil@springboard.com>
2019-07-15 23:30:33 +0530

    1. added deploy scripts 2. added a config for the boilerplate text 3. added text using config to the background text component 4. make height: 100%

---
+++ src/boilerplate-text.js
@@ -0,0 +1,38 @@
+const boilerplate = {
+  text : 'It won’t be a bigger problem to find one video game lover in your neighbor. Since the introduction of Virtual\n' +
+    '          Game, it has been achieving great heights so far as its popularity and technological advancement are\n' +
+    '          concerned. The history of video game is as interesting as a fairy tale. The quality of today’s video game was\n' +
+    '          not at all there when video game first conceptualized and played ever. During the formulative years, video\n' +
+    '          games were created by using various interactive electronic devices with various display formats. The first\n' +
+    '          ever video game was designed in 1947 by Thomas T. Goldsmith Jr. and Estle Ray Mann. (Source: Wikipedia.org)\n' +
+    '          The device was recognized as Cathode Ray Tube Amusement Device. From Cathode Ray Tube Amusement Device to Xbox\n' +
+    '          360, PlayStation series or Nintendo WII, the story behind video games and accessories is really very creative\n' +
+    '          and adventurous. Recently played video games are quite developed and they are more interactive than the\n' +
+    '          earlier video games. From analog video game design to 2D and 3D video games all have their own charm and video\n' +
+    '          game lovers try to play all sorts of video games. In this respect our team talked to John, who lives in\n' +
+    '          Sidney, Australia. When he was asked about his preference about the type of video game, he replied, “Let me\n' +
+    '          make it clear from the very beginning that I’m really crazy about video games. I’ve been playing video games\n' +
+    '          since I gained consciousness…LOL…Really I’m serious. My father owns a video game store here in Sidney and we\n' +
+    '          are still managing the same store. The only difference that you may find is the shelves. The old analog video\n' +
+    '          games were replaced by 2D and 3D video games. We have several regular customers and they very often come to\n' +
+    '          our store and ask for the LATEST. I too ask about their preference and they say they love to try any sort of\n' +
+    '          video games. What ever that is competitive, they love to play. So far playing excitement of video games is\n' +
+    '          concerned, they can be compared with the soccer match between Manchester United and Liverpool.” It’s not the\n' +
+    '          quality of video games that improves, researches and developments are too evident in video game accessories,\n' +
+    '          too. Without such accessories, the real excitement of video games can not be enjoyed. It’s pretty easy to\n' +
+    '          recognize video game accessories. The video game accessories are just like computer hard wares. If video games\n' +
+    '          are software, the most important video game accessories are consoles and D-pad. Currently several companies\n' +
+    '          are manufacturing video game accessories. Some of the major players in the industry are Microsoft, Sony and\n' +
+    '          Nintendo. Microsoft’s Xbox 360, Sony’s PlayStation series and Nintendo’s WII are high in demand. Xbox 360 is\n' +
+    '          the second video game console introduced by Microsoft in the video game industry. This was developed in\n' +
+    '          association with IBM, ATI and SiS. Xbox Live service enables players to compete online as well as players can\n' +
+    '          download other relevant content from internet. Xbox 360 is the seventh generation game console and currently\n' +
+    '          challenged by Sony’s Play Station 3 and Nintendo’s WII in the market. Xbox 360 has broken several video market\n' +
+    '          records as it has completely been sold out on its first launch except in Japan. Till January 3, 2008, 17.7\n' +
+    '          million pieces are sold out with 6.3 million in the United States only. Are you feeling the heat of video\n' +
+    '          game? Then why delaying the purchase, go to your nearest BlueBearToys store or you can order online and can\n' +
+    '          get better deal and great savings on your purchase! Logon to BlueBearToys.com (Electronic and Gadgets\n' +
+    '          Superstore) and order for Xbox 360 and other video game consoles NOW!'
+};
+
+export default boilerplate;
