e1bde2e21ad8e40655d9a8f553cdd0020b0ef124
Rimil Dey <rimil@springboard.com>
2019-07-17 21:52:23 +0530

    Updates

---
+++ precache-manifest.e6af8d9a4b697584e4fd3a55eb9388cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0eafac62664bca5fb7073d9418e257",
+    "url": "/landing-page-ui/index.html"
+  },
+  {
+    "revision": "6355a6a93731d0030dfc",
+    "url": "/landing-page-ui/static/css/main.858b74bf.chunk.css"
+  },
+  {
+    "revision": "cf852ab8779b47375e54",
+    "url": "/landing-page-ui/static/js/2.8dc0e453.chunk.js"
+  },
+  {
+    "revision": "6355a6a93731d0030dfc",
+    "url": "/landing-page-ui/static/js/main.c974ffaf.chunk.js"
+  },
+  {
+    "revision": "d7addbc357a314b72293",
+    "url": "/landing-page-ui/static/js/runtime~main.0550c504.js"
+  },
+  {
+    "revision": "f322cb5a8241ef68dfdcaf7811c565da",
+    "url": "/landing-page-ui/static/media/baseline-account_circle.f322cb5a.svg"
+  },
+  {
+    "revision": "5882f300cebd34e4e1cd27dcfb9de0fc",
+    "url": "/landing-page-ui/static/media/baseline-clear.5882f300.svg"
+  },
+  {
+    "revision": "fbd587d106000984d9402802ff84e58e",
+    "url": "/landing-page-ui/static/media/baseline-email.fbd587d1.svg"
+  },
+  {
+    "revision": "bb158ab64ed266b995914babb7ecf46a",
+    "url": "/landing-page-ui/static/media/baseline-expand.bb158ab6.svg"
+  }
+]);
